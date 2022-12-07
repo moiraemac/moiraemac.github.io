2798fd43d4bc151b3d29302b4cdd44e178897d66
David Walker <walk.dv@gmail.com>
2022-12-06 23:14:12 -0500

    Updates

--- src/hooks/isNarrow.tsx
+++
@@ -1,7 +0,0 @@
-import useDimensions from "./useDimensions"
-
-const useIsNarrow = (minWidth: number): boolean => {
-    const {width} = useDimensions();
-    return width < minWidth;
-};
-export default useIsNarrow;
