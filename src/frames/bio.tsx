020d8c6cef10e9b9dfc91ac97350a659c450bee7
Moira MacNeil <moiramacneil@Hameds-Mac-mini.local>
2023-11-09 19:04:38 -0500

    Update bio to include Antoine

--- src/frames/bio.tsx
+++ src/frames/bio.tsx
@@ -16,7 +16,7 @@ function ListFrame(p: {includeContact: boolean}) {
                     </span>
                 </div>
             </>}
-            <p>I am a PhD Candidate at the University of Toronto under the supervision of Professor Merve Bodur. Broadly, my research uses theory and computation to solve combinatorial and discrete optimization problems and answer geometric and combinatorial questions about convex polytopes. I am interested in applications in biology, decision-making under uncertainty, and social good. My current research focuses on the theory, methodology, and algorithms used to determine the diameter of lattice polytopes.</p>
+            <p>I am a PhD Candidate at the University of Toronto under the supervision of Professors Merve Bodur and Antoine Deza. Broadly, my research uses theory and computation to solve combinatorial and discrete optimization problems and answer geometric and combinatorial questions about convex polytopes. I am interested in applications in biology, decision-making under uncertainty, and social good. My current research focuses on the theory, methodology, and algorithms used to determine the diameter of lattice polytopes.</p>
             
         </div>
     )
