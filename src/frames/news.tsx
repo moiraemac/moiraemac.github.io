1fd0bb0c929547b92ed65691e231cdbefeea999f
Moira MacNeil <moiraemacneil@gmail.com>
2023-11-10 10:46:49 -0500

    Updated news and teaching sections

--- src/frames/news.tsx
+++ src/frames/news.tsx
@@ -6,6 +6,8 @@ import allSentences from '../fixture/sentences'
 const frameName = "News";
 
 const sentences = [
+    "Fall 2023: I am on the academic job market"
+    "August 2023: I completed Prospective Professors in Training (PPIT) program at UofT",
     "September 2022: I have been accepted into the Prospective Professors in Training (PPIT) program at UofT",
     "July 2022: I was awarded the Ontario Graduate Scholarship",
 ];
--- src/frames/teaching.tsx
+++ src/frames/teaching.tsx
@@ -6,8 +6,10 @@ function ListFrame() {
     return (
         <div className="Frame List-Frame" id={"teaching"}>
             <h2>Teaching</h2>
+            <h4>MIE245: Data Structures and Algorithms</h4>
+            <p>University of Toronto, Teaching Assistant, 2024</p>
             <h4>MIE1603/1653: Integer Programming</h4>
-            <p>University of Toronto, Teaching Assistant, 2020-2022</p>
+            <p>University of Toronto, Teaching Assistant, 2020-2023</p>
             <h4>CSCI1101: Computer Science II</h4>
             <p>Dalhousie University, Teaching Assistant, 2016-2017</p>
             <h4>CSCI2110: Computer Science III</h4>
