c4e44f613894fd5761ad6f52b00b61fb1684fee8
Moira MacNeil <moiraemacneil@gmail.com>
2024-02-14 14:10:40 -0500

    Update BDD publication

--- src/frames/research.tsx
+++ src/frames/research.tsx
@@ -10,11 +10,13 @@ function ListFrame() {
             <ul>
                 <li>M. MacNeil*, and M. Bodur, <a href='https://doi.org/10.1002/net.22068'>"Constraint Programming Approaches to the Discretizable Molecular Distance Geometry Problem"</a>, Networks, 79(4): 515-536, 2022.</li>
                 <li>M. MacNeil*, and M. Bodur, <a href='https://doi.org/10.1287/ijoc.2020.1039'>"Integer Programming, Constraint Programming, and Hybrid Decomposition Approaches to Discretizable Distance Geometry Problems"</a>, INFORMS Journal on Computing, 34(1): 297-314, 2022.</li>
+                <li>M. MacNeil*, and M. Bodur, <a href="https://doi.org/10.1016/j.ejor.2023.12.021">"Leveraging Decision Diagrams to Solve Two-stage Stochastic Programs with Binary Recourse and Logical Linking Constraints"</a>,  In Press, European Journal of Operational Research, 2023.</li>
+
             </ul>
-            <h3>Preprints</h3>
+{/*             <h3>Preprints</h3>
             <ul>
                 <li>M. MacNeil*, and M. Bodur, <a href="https://optimization-online.org/wp-content/uploads/2022/11/BDD_2SP.pdf">"Leveraging Decision Diagrams to Solve Two-stage Stochastic Programs with Binary Recourse and Logical Linking Constraints"</a>, 2022.</li>
-            </ul>
+            </ul> */}
         </div>
     )
 }
